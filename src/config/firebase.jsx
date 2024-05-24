71ebbec3954f9ee6815ea8824f8549b5a8b38b3a
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-05-24 12:49:50 +0500

    add some changess

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -143,8 +143,6 @@ const getRealTimeBookedTime = async (reservationDate, reservationLocation, dateO
 
     const arr = [{ time: "00-00 01-00" }, { time: "01-00 02-00" }, { time: "02-00 03-00" }, { time: "03-00 04-00" }, { time: "04-00 05-00" }, { time: "05-00 06-00" }, { time: "06-00 07-00" }, { time: "07-00 08-00" }, { time: "08-00 09-00" }, { time: "9-00 10-00" }, { time: "10-00 11-00" }, { time: "11-00 12-00" }, { time: "12-00 13-00" }, { time: "13-00 14-00" }, { time: "14-00 15-00" }, { time: "15-00 16-00" }, { time: "16-00 17-00" }, { time: "17-00 18-00" }, { time: "18-00 19-00" }, { time: "19-00 20-00" }, { time: "20-00 21-00" }, { time: "21-00 22-00" }, { time: "22-00 23-00" }, { time: "23-00 24-00" }];
 
-    const timeArr = arr.slice(dateObj.getHours());
-
     const appointmentCollection = collection(db, "appointment");
 
     const q = reservationLocation ? query(
@@ -154,12 +152,14 @@ const getRealTimeBookedTime = async (reservationDate, reservationLocation, dateO
     ) : appointmentCollection;
 
     onSnapshot(q, res => {
+    const timeArr = arr.slice(dateObj.getHours());
 
         timeArr.forEach(timeElement => {
                 timeElement.booked = 0 ;
         });
 
         res.docs.forEach(element => {
+            
                 timeArr.forEach(timeElement => {
                 if (element.data().time == timeElement.time) {
                     timeElement.booked = timeElement.booked + 1;
